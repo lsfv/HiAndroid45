c81ebb2847fd6ef27e95aae53dfc80d4ed507d07
lsfv <lsfv@163.com>
2019-09-11 12:08:36 +0800

    v1

--- app/src/main/java/com/linson/android/hiandroid2/Services/Index.java
+++ app/src/main/java/com/linson/android/hiandroid2/Services/Index.java
@@ -7,9 +7,12 @@ import android.widget.Button;
 
 import com.linson.LSLibrary.AndroidHelper.LSComponentsHelper;
 import com.linson.android.hiandroid2.R;
+import com.linson.android.hiandroid2.Services.AIDLSys.SysAidl;
 
 public class Index extends AppCompatActivity implements View.OnClickListener
 {
+    private Button mBtnAIDL2;
+    private Button mBtnAIDL3;
     private Button mBtnSocket;
     private Button mBtnSingle;
     private Button mBtnDownloadservices;
@@ -18,12 +21,11 @@ public class Index extends AppCompatActivity implements View.OnClickListener
     private Button mBtnHandler;
     private Button mBtnAIDL;
 
-
-
-
     //region  controls and click event
     private void findControls()
     {
+        mBtnAIDL2 = (Button) findViewById(R.id.btn_AIDL2);
+        mBtnAIDL3 = (Button) findViewById(R.id.btn_AIDL3);
         mBtnSocket = (Button) findViewById(R.id.btn_Socket);
         mBtnSingle = (Button) findViewById(R.id.btn_single);
         mBtnDownloadservices = (Button) findViewById(R.id.btn_downloadservices);
@@ -39,6 +41,8 @@ public class Index extends AppCompatActivity implements View.OnClickListener
         mBtnSingle.setOnClickListener(this);
         mBtnAIDL.setOnClickListener(this);
         mBtnSocket.setOnClickListener(this);
+        mBtnAIDL2.setOnClickListener(this);
+        mBtnAIDL3.setOnClickListener(this);
     }
 
     @Override
@@ -48,7 +52,7 @@ public class Index extends AppCompatActivity implements View.OnClickListener
         {
             case R.id.btn_handler:
             {
-                LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.HandlerPractice.class);
+               // LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.HandlerPractice.class);
                 break;
             }
             case R.id.btn_async:
@@ -58,22 +62,28 @@ public class Index extends AppCompatActivity implements View.OnClickListener
             }
             case R.id.btn_downloadservices:
             {
-                LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.DownloadService.class);
+               // LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.DownloadService.class);
                 break;
             }
             case R.id.btn_single:
             {
-                LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SingleServices.class);
+                //LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SingleServices.class);
                 break;
             }
             case R.id.btn_AIDL:
             {
-                LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SaleQuery.class);
+                //LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SaleQuery.class);
                 break;
             }
             case R.id.btn_Socket:
             {
-                LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SocketPracice.class);
+                //LSComponentsHelper.startActivity(this, com.linson.android.hiandroid2.Services.SocketPracice.class);
+                LSComponentsHelper.startActivity(this, SocketPractice.class);
+                break;
+            }
+            case R.id.btn_AIDL2:
+            {
+                LSComponentsHelper.startActivity(this, SysAidl.class);
                 break;
             }
             default:
